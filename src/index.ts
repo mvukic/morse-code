0f0a187544274f8fa195dfb13b2be83cfad7ae59
mvukic <1702367+mvukic@users.noreply.github.com>
2019-11-05 19:27:30 +0100

    start parsing arguments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import fs from 'fs';
-
 import { letterToCodeMap } from './alphabet';
+import { Arguments, getArguments } from './arguments';
+import * as yargs from 'yargs';
 
 async function start() {
-  const input: string = await fs.promises.readFile('./input/input.txt', { encoding: 'utf8' });
-  const coded = input.split('').map((letter) => {
-    const code = letterToCodeMap.get(letter.toUpperCase());
-    return code ? code : letter;
-  }).join('');
-  console.log(coded);
+  const config = getArguments();
+  read(config);
+}
+
+function read(config: Arguments) {
+  
+}
+
+function process() {
+//   const input: string = await fs.promises.readFile('./input/input.txt', { encoding: 'utf8' });
+//   const coded = input.split('').map((letter) => {
+//     const code = letterToCodeMap.get(letter.toUpperCase());
+//     return code ? code : letter;
+//   }).join('');
+}
+
+function write(config: Arguments, output: string) {
+  console.log(output);
 }
 
 start();
